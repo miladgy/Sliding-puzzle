b86bd575fd3e6c396fb3ad81663495fdc1981521
Milad Ghadami <milad.ghadami@appliedtechnology.se>
2019-10-31 16:38:19 +0100

    fix layout and mix cards up

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Sliding-puzzle
+
+This is a game of sliding puzzle.
+Currently, the layout seems to be good but needs more work!
+span tags have been added so one can change from picture puzzle to 8 game puzzle using numbers!
+Mixing the cards/cells functionality has been implemented!
--- index.js
+++ index.js
@@ -1,19 +1,46 @@
-const puzzleSize = document.getElementsByClassName('puzzleSize')[0];
+// const puzzleSize = document.getElementsByClassName('puzzleSize')[0];
+const mixBtn = document.querySelector('.mix-btn')
+const switchCells = (oldCell, newCell) => {
+  let inBetween = document.getElementsByClassName(oldCell)[0].className;
+  document.getElementsByClassName(oldCell)[0].className = document.getElementsByClassName(newCell)[0].className;
+  document.getElementsByClassName(newCell)[0].className = inBetween;
 
+}
+const mixPuzzle = () => {
+  for (col = 1; col < 4; col++){
+    for (row = 1; row < 4; row++){
+      const newCol = Math.floor(Math.random() * 3 + 1);
+      const newRow = Math.floor(Math.random() * 3 + 1);
+      switchCells(`cell${col}${row}`, `cell${newCol}${newRow}`);
+    }
+  }
+}
+
+mixBtn.addEventListener('click', () => {
+  console.log('i am here in button handler')
+  mixPuzzle();
+})
 
 window.onload = () => {
-  // sizeSubmitBtn.addEventListener("click", (e) => {
-    // e.preventDefault();
     fetch('https://source.unsplash.com/random/564x564')
     .then((res) => {
-      document.querySelector('.img').src = 
-      `${res.url}`;
       for (i = 1; i < 4; i++){
-          for (j = 1; j < 4; j++){
+        for (j = 1; j < 4; j++){
+          const image = document.createElement('img');
+          image.src = `${res.url}`;
           document.querySelector(`.cell${i}${j}`).style.backgroundImage =
           `url(${res.url})`;
+          document.getElementsByTagName('body')[0].style.backgroundImage = `url(${res.url})`;
+        
         }
       }
+    })
+//     sizeSubmitBtn.addEventListener("click", (e) => {
+//     e.preventDefault();
+// })
+    const table = document.querySelector('.table');
+    table.addEventListener('click', (e) => {
+      
     })
 }
 // for (i = 0; i < puzzleSize; i++) {
