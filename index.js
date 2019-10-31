30bdc3e6c84d683520fb181982c50f62c91d0d3f
Milad Ghadami <milad.ghadami@appliedtechnology.se>
2019-10-31 12:11:44 +0100

    second commit fixed puzzle layout

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const puzzleSize = document.getElementsByClassName('puzzleSize')[0];
-const sizeSubmitBtn = document.getElementsByClassName('submitSize')[0];
+
 
 window.onload = () => {
-  sizeSubmitBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    const imgW = document.getElementsByClassName('imgW')[0].value;
-    const imgH = document.getElementsByClassName('imgH')[0].value;
-    document.getElementById('img').src = `https://source.unsplash.com/random/${imgW}x${imgH}`; 
-    console.log();
-  });
+  // sizeSubmitBtn.addEventListener("click", (e) => {
+    // e.preventDefault();
+    fetch('https://source.unsplash.com/random/564x564')
+    .then((res) => {
+      document.querySelector('.img').src = 
+      `${res.url}`;
+      for (i = 1; i < 4; i++){
+          for (j = 1; j < 4; j++){
+          document.querySelector(`.cell${i}${j}`).style.backgroundImage =
+          `url(${res.url})`;
+        }
+      }
+    })
 }
-for (i = 0; i < puzzleSize; i++) {
-  window[i] = document.createElement('div');
-  rowOneDiv.className = `row${i}`;
-  rowOneDiv.style.display = 'tabel-row';
-}
-console.log('constant name', window[2]);
+// for (i = 0; i < puzzleSize; i++) {
+//   window[i] = document.createElement('div');
+//   rowOneDiv.className = `row${i}`;
+//   rowOneDiv.style.display = 'tabel-row';
+// }
+// console.log('constant name', window[2]);
 
 
